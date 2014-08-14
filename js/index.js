24aec69c7b76836935e86b8435f98ff57cc71295
Wei <ray789520@gmail.com>
2014-08-14 15:50:36 +0800

    inedx.js

--- js/index.js
+++ js/index.js
@@ -3,168 +3,96 @@ $(document).ready(function(){
 		revision = law_history.revision; 
 		diffString(revision);
 	});
-	function diffString(revision){
-		for(var k=0; k<revision.length-1; k++){
-			var obj_n = revision[k+1].content;
-			for(var key in obj_n){
-				num_n =	obj_n[key];
-				article_n = revision[k+1].content[num_n.num].article.split("");
-				var date = revision[k+1].date;
-				if(revision[0].content[num_n.num] == null){
-					article_o = "";
-					var out = diff(article_o,article_n);
-					var str = "";
-					for(var m=0; m < out[1].length; m++){
-						if(m==0)
-							str += '<ins>';
-						str += out[1][m];
-						if(m==out[1].length-1)
-							str += "</ins>";
+	/*	function randomColor() {
+		return "rgb(" + (Math.random() * 100) + "%, " + 
+		(Math.random() * 100) + "%, " + 
+		(Math.random() * 100) + "%)";
+		}*/
+		function diffString2(o,n){
+			var out = diff(o,n);
+			var str = "";
+			if(out.n.length == 0){
+				for (var i = 0; i < out.o.length; i++){
+					str += '<del>' + out.o[i] + "</del>";
+				}
+			}
+			else {
+				if (out.n[0].text == null){
+					for (n = 0; n < out.o.length && out.o[n].text == null; n++){
+						str += '<del>' + out.o[n] + "</del>";
 					}
+				}
+				for(var i = 0; i < out.n.length; i++){
+					if(out.n[i].text == null){
+						str += '<ins>'+ out.n[i] + "</ins>";
+					}
+					else {
+						var pre = "";
+						for (n = out.n[i].row + 1; n < out.o.length && out.o[n].text == null; n++){
+							pre += '<del>' + out.o[n] + "</del>";
+						}
+						str += out.n[i].text + pre;
+					}
+				}
+			} 
+			// return str;
+		}
+		function diffString(revision){
+			for(var k=0; k<revision.length-1; k++){
+				var obj_n = revision[k+1].content;
+				for(var key in obj_n){
+					num_n =	obj_n[key];
+					article_n = revision[k+1].content[num_n.num].article.split("");
+					var date = revision[k+1].date;		
+					if(revision[0].content[num_n.num] == null){
+						article_o = "";
+						console.log(num_n.num);
+						diffString2(article_o, article_n);
+						continue;
+					}
+					else
+						article_o = revision[0].content[num_n.num].article.split("");
 					console.log(num_n.num);
-					console.log(date);
-					console.log(str);
-					continue;
+					diffString2(article_o, article_n);	
 				}
-				else {
-					article_o = revision[0].content[num_n.num].article.split("");
-				}
-				var out = diff(article_o,article_n);
-				var str = "";
-				var i = 0;
-				if(out[0].length > out[1].length){
-					for(var i=0; i < out[0].length; i++){
-						if(i < out[1].length){
-							if(out[1][i].text == null){
-								var tmp = i;
-								for(var j = tmp; j < out[1].length; j++){
-									if(tmp == j)
-										str += '<del>';
-									str += out[0][j];
-									if(out[0][j+1].text != null || j==out[1].length-1){
-										str += "</del>";
-										i=j;
-										break;
-									}
-								}
-								for(var j = tmp; j < out[1].length; j++){
-									if(tmp==j)
-										str += '<ins>';
-									str += out[1][j];
-									if(out[0][j+1].text != null || j==out[1].length-1){
-										str += "</ins>";
-										i=j;
-										break;
-									}
-								}
-							}
-							else {
-								for(var j = i ;j < out[1].length; j++){
-									if(j==i)
-										str += '<ins>';
-									str += out[0][j].text;
-									if(out[0][j+1].text == null || j==out[1].length-1){
-										str += "</ins>";
-										i=j;
-										break;
-									}
-								}
-							}
-						}
-						else{
-							for(var j =i; j < out[0].length; j++){
-								if(j==i)
-									str += '<del>';
-								str += out[0][j];
-								if(j==out[0].length-1){
-									str += "</del>";
-									i=j;
-									break;
-								}
-							}
-						}
-					}
-				}	
-				else{
-					for(var i=0; i < out[1].length; i++){
-						if(i < out[0].length){
-							if(out[0][i].text == null){
-								var tmp = i;
-								for(var j = tmp; j < out[0].length; j++){
-									if(tmp == j)
-										str += '<del>';
-									str += out[0][j];
-									if(out[1][j+1].text != null || j==out[0].length-1){
-										str += "</del>";
-										i=j;
-										break;
-									}
-								}
-								for(var j = tmp; j < out[0].length; j++){
-									if(tmp==j)
-										str += '<ins>';
-									str += out[1][j];
-									if(out[1][j+1].text != null || j==out[0].length-1){
-										str += "</ins>";
-										i=j;
-										break;
-									}
-								}
-							}
-							else{
-								for(var j = i ;j < out[0].length; j++){
-									if(j==i)
-										str += '<ins>';
-									str += out[0][j].text;
-									if(out[1][j+1].text == null || j==out[0].length){
-										str += "</ins>";
-										i=j;
-										break;
-									}
-								}
-							}
-						}
-						else{
-							for(var j =i; j < out[1].length; j++){
-								if(j==i)
-									str += '<ins>';
-								str += out[1][j];
-								if(j==out[1].length-1){
-									str += "</ins>";
-									i=j;
-									break;
-								}
-							}
-						}
-					} 
-				}
-				console.log(num_n.num);
-				console.log(date);
-				console.log(str);
 			}
 		}
-	}
-
-	function diff(o,n){
-		for(var i=0; i < o.length; i++){
-			if(o[i] == n[i])
-				o[i] = {text: o[i], rows: i};	
-			else
-				o[i] = o[i];
-		}
-		for(var i=0; i < n.length; i++){
-			if(i < o.length){
-				if(o[i].text != null && o[i].rows == i)
-					n[i] = {text: n[i], rows: i};
-				else
-					n[i] = n[i];
+		function diff( o, n ) {
+			var ns = new Object();
+			var os = new Object();
+			console.log(o);
+			for ( var i = 0; i < n.length; i++ ) {
+				if ( ns[ n[i] ] == null )
+					ns[ n[i] ] = { rows: new Array(), o: null };
+				ns[ n[i] ].rows.push( i );
 			}
-			else 
-				n[i] = n[i];
+			for ( var i = 0; i < o.length; i++ ) {
+				if ( os[ o[i] ] == null )
+					os[ o[i] ] = { rows: new Array(), n: null };
+				os[ o[i] ].rows.push( i );
+			}
+			for ( var i in ns ) {
+				if ( ns[i].rows.length == 1 && typeof(os[i]) != "undefined" && os[i].rows.length == 1 ) {
+					n[ ns[i].rows[0] ] = { text: n[ ns[i].rows[0] ], row: os[i].rows[0] };
+					o[ os[i].rows[0] ] = { text: o[ os[i].rows[0] ], row: ns[i].rows[0] };
+				}
+			}
+			for ( var i = 0; i < n.length - 1; i++ ) {
+				if ( n[i].text != null && n[i+1].text == null && n[i].row + 1 < o.length && o[ n[i].row + 1 ].text == null && 
+				n[i+1] == o[ n[i].row + 1 ] ) {
+					n[i+1] = { text: n[i+1], row: n[i].row + 1 };
+					o[n[i].row+1] = { text: o[n[i].row+1], row: i + 1 };
+				}
+			}
+			for ( var i = n.length - 1; i > 0; i-- ) {
+				if ( n[i].text != null && n[i-1].text == null && n[i].row > 0 && o[ n[i].row - 1 ].text == null && 
+				n[i-1] == o[ n[i].row - 1 ] ) {
+					n[i-1] = { text: n[i-1], row: n[i].row - 1 };
+					o[n[i].row-1] = { text: o[n[i].row-1], row: i - 1 };
+				}
+			}
+			return { o: o, n: n };
 		}
-		console.log(o);
-		console.log(n);
-		return [o,n] ;
-	}
 })
+
 
